409a29f8a53add1e41fc255cbe1f74da9b39632b
Yahor M <chegor@gmail.com>
2020-07-22 16:44:09 +0300

    final

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,8 +1,6 @@
 from .base_page import BasePage
 from .locators import LoginPageLocators
 
-import time
-
 class LoginPage(BasePage):
     def should_be_login_page(self):
         self.should_be_login_url()
@@ -22,10 +20,11 @@ class LoginPage(BasePage):
     def register_new_user(self, email, password):
         email_field = self.browser.find_element(*LoginPageLocators.REG_FORM_EMAIL_INPUT)
         email_field.send_keys(email)
-        #self.browser.execute_script("return arguments[0].scrollIntoView(true);", email_field)
         pass1_field = self.browser.find_element(*LoginPageLocators.REG_FORM_1_PASS)
         pass1_field.send_keys(password)
         pass2_field = self.browser.find_element(*LoginPageLocators.REG_FORM_2_PASS)
         pass2_field.send_keys(password)
         button = self.browser.find_element(*LoginPageLocators.REG_FORM_SUBMIT_BUTTON)
         button.click()
+
+
